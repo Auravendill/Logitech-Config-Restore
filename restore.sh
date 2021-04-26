ec8ac281a4ba55b7d039ea793d6fd8a8214f47b6
Auravendill <auravendill@gmx.de>
2021-04-26 02:03:46 +0200

    removed ' ' added by ide

--- restore.sh
+++ restore.sh
@@ -1,7 +1,7 @@
 g910-led -a 0000ff
 g910-led -g fkeys 00ff00
 g910-led -g gkeys 808000
-g910-led - k w ff0000
-g910-led - k a ff0000
-g910-led - k s ff0000
-g910-led - k d ff0000
+g910-led -k w ff0000
+g910-led -k a ff0000
+g910-led -k s ff0000
+g910-led -k d ff0000
